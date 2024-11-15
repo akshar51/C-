2b4c1f6b37ce3e9bfd94abdf62df281ec2fd6cac
akshar51 <aksharparekh401@gmail.com>
2024-11-15 17:26:48 +0530

    done

--- Cpp/first.cpp
+++ Cpp/first.cpp
@@ -7,6 +7,8 @@ int main(){
     cout<<"C plus"<<endl;
     cout<<"Enter number :";
     cin>>a;
-    cout<<"Value of a is :"<<a;
+    cout<<"Value of a is :"<<a<<endl;
+    int b=10,c=20;
+    cout<<b+c;
     return 0;
 }
