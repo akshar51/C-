0e381759e134595a6d19347c3172aa211071d5cd
akshar51 <aksharparekh401@gmail.com>
2024-11-15 17:42:04 +0530

    done

--- Cpp/demo/first.cpp
+++ Cpp/demo/first.cpp
@@ -4,11 +4,18 @@ using namespace std;
 int main(){
     int a;
     cout<<"Hello world"<<endl;
-    cout<<"C plus"<<endl;
-    cout<<"Enter number :";
-    cin>>a;
-    cout<<"Value of a is :"<<a<<endl;
-    int b=10,c=20;
-    cout<<b+c;
+   // cout<<"C plus"<<endl;
+    //cout<<"Enter number :";
+    //cin>>a;
+    //cout<<"Value of a is :"<<a<<endl;
+    //int b=10,c=20;
+    //cout<<b+c;
+
+    for( int i=1;i<=5;i++){
+        for(int j=1;j<i+1;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
     return 0;
 }
