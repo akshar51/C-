8ef4f0959e6c653da87547ec11c8df0f043a1331
akshar51 <aksharparekh401@gmail.com>
2024-11-15 17:23:16 +0530

    Done

---
+++ Cpp/first.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Hello world"<<endl;
+    cout<<"C plus"<<endl;
+    cout<<"Enter number :";
+    cin>>a;
+    cout<<"Value of a is :"<<a;
+    return 0;
+}
