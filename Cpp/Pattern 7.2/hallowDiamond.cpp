42de5c959558c7c37eb7a8bbd3169c1d3e5ace39
akshar51 <aksharparekh401@gmail.com>
2024-11-27 22:02:49 +0530

    done

---
+++ Cpp/Pattern 7.2/hallowDiamond.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 1 2 3 4 5 5 4 3 2 1
+    // 1 2 3 4 - - 4 3 2 1
+    // 1 2 3 - - - - 3 2 1
+    // 1 2 - - - - - - 2 1
+    // 1 - - - - - - - - 1
+    // 1 - - - - - - - - 1
+    // 1 2 - - - - - - 2 1
+    // 1 2 3 - - - - 3 2 1
+    // 1 2 3 4 - - 4 3 2 1
+    // 1 2 3 4 5 5 4 3 2 1
+
+    for (int i = 5; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        for (int k = 0; k < 2 * (5 - i); k++)
+        {
+            cout << "  ";
+        }
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        for(int k=5;k>i;k--){
+            cout<<"    ";
+        }
+        for(int j=i;j>=1;j--){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
