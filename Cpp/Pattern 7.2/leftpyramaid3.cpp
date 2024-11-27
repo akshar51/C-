f7088568f9c2aa34e57071c51e21f1f4f19597fd
akshar51 <aksharparekh401@gmail.com>
2024-11-27 16:59:12 +0530

    done

---
+++ Cpp/Pattern 7.2/leftpyramaid3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //- - - - 5
+    //- - - 4 4
+    //- - 3 3 3
+    //- 2 2 2 2
+    //1 1 1 1 1
+
+    for(int i=5;i>=1;i--){
+        for(int k=1;k<i;k++){
+             cout<<"  ";
+        }
+        for(int j=i;j<=5;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+
+
+
+
