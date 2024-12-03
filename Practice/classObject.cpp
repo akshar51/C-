d9a94e2dcd3a897de71ee206425e32db4cc6d159
akshar51 <aksharparekh401@gmail.com>
2024-12-03 17:40:48 +0530

    done

---
+++ Practice/classObject.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+
+class Student{
+    public:
+    int roll;
+    char name[50];
+    float percentage;
+};
+
+int main(){
+    Student stu1,stu2;
+    stu1.roll=99;
+    strcpy(stu1.name,"Akshar");
+
+    cout<<"Roll number is : "<<stu1.roll<<endl;
+    cout<<"Name is : "<<stu1.name<<endl;
+    
+    stu2.roll=100;
+    strcpy(stu2.name,"Rahul");
+
+    cout<<"Roll number is : "<<stu2.roll<<endl;
+    cout<<"Name is : "<<stu2.name<<endl;
+
+    stu1.roll=101;
+    cout<<"Roll number is : "<<stu1.roll<<endl;
+    
+
+}
