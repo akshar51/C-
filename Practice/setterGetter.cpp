ee3bc935e11657e7cdcd0cd5f19be8da5647afc5
akshar51 <aksharparekh401@gmail.com>
2024-12-04 17:38:44 +0530

    done

--- Practice/setterGetter.cpp
+++ Practice/setterGetter.cpp
@@ -2,40 +2,66 @@
 #include<string>
 using namespace std;
 
-class Student{
+// class Student{
+//     private:
+//     int grid;
+//     string name;
+//     int age;
+
+//     public:
+//     //Setter
+//     void setdata(int grid,string name,int age){
+//         this->grid=grid;
+//         this->name=name;
+//         this->age=age;
+//     }
+
+//     //Getter
+//     void getdata(){
+//         cout<<"Grid: "<<grid<<" "
+//             <<"Name: "<<name<<" "
+//             <<"Age: "<<age<<endl;
+//     }
+
+//     Student *getcurrent(){
+//         return this;
+//     }
+
+// };
+
+class Bank{
     private:
-    int grid;
-    string name;
-    int age;
+    int balance=1000;
 
     public:
-    //Setter
-    void setdata(int grid,string name,int age){
-        this->grid=grid;
-        this->name=name;
-        this->age=age;
+    void code(int x){
+        if(x==123){
+            cout<<"Account balance is : "<<balance;
+        }
+        else{
+            cout<<"Invalid code....";
+        }
     }
-
-    //Getter
-
-    void getdata(){
-        cout<<"Grid: "<<grid<<" "
-            <<"Name: "<<name<<" "
-            <<"Age: "<<age;
-    }
-
 };
 
 
 
 int main(){
 
-    Student stud1,stud2,stud3;
+    // Student stud1,stud2,stud3;
 
-    stud1.setdata(777,"Akshar",20);
-    stud2.setdata(888,"Nyan",55);
+    // stud1.setdata(777,"Akshar",20);
+    // stud2.setdata(888,"Nyan",55);
+
+    // stud1.getdata();
+    // stud2.getdata();
     
-    
+    // stud3=*stud1.getcurrent();
+    // stud3.getdata();
+   
+   Bank bal;
+   bal.code(123);
+
    
 
 
