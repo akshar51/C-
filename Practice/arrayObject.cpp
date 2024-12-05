b4e976dbc1be24001358814083b053992e04db2e
akshar51 <aksharparekh401@gmail.com>
2024-12-05 18:04:05 +0530

    done

--- Practice/arrayObject.cpp
+++ Practice/arrayObject.cpp
@@ -2,42 +2,49 @@
 #include<string>
 using namespace std;
 
-class Student{
-   public:
-    int roll;
-    string name;
-    int age;
+
+int x=100;
+
+// class Student{
+//    public:
+//     int roll;
+//     string name;
+//     int age;
 
     
-    void setdata(){
-        cout<<"Roll no:";
-        cin>>roll;
-        cout<<"Name:";
-        cin>>name;
-        cout<<"Age:";
-        cin>>age;
-    }
+//     void setdata(){
+//         cout<<"Roll no:";
+//         cin>>roll;
+//         cout<<"Name:";
+//         cin>>name;
+//         cout<<"Age:";
+//         cin>>age;
+//     }
 
-    void getdata(){
-        cout<<"Roll:"<<roll<<" "
-            <<"Name:"<<name<<" "
-            <<"Age:"<<age<<endl;
-    }
-};
+//     void getdata(){
+//         cout<<"Roll:"<<roll<<" "
+//             <<"Name:"<<name<<" "
+//             <<"Age:"<<age<<endl;
+//     }
+// };
 
 
 int main(){
-    int size;
-    cout<<"Enter number of student : ";
-    cin>>size;
-    Student stud[size];
+    // int size;
+    // cout<<"Enter number of student : ";
+    // cin>>size;
+    // Student stud[size];
 
-    for(int i=0;i<size;i++){
-        stud[i].setdata();
-    }
+    // for(int i=0;i<size;i++){
+    //     stud[i].setdata();
+    // }
 
-    for(int i=0;i<size;i++){
-        stud[i].getdata();
+    // for(int i=0;i<size;i++){
+    //     stud[i].getdata();
        
-    }
+    // }
+
+    int x=50;
+    cout<<"Global x = "<<::x<<endl;
+    cout<<"Local x = "<<x<<endl;
 }
