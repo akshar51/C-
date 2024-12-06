64e7bcd1b6e392d13b6e03d139c0234b1edd25d3
akshar51 <aksharparekh401@gmail.com>
2024-12-06 17:37:15 +0530

    done

--- Practice/constructor.cpp
+++ Practice/constructor.cpp
@@ -1,6 +1,36 @@
 #include<iostream>
 using namespace std;
 
+class City{
+    public:
+    int id;
+
+    // DEFAULT CONSTRUCTOR
+    City(){
+        this->id=5;
+        cout<<"ID is : "<<id<<endl;      
+    }
+
+    //PARAMETERIZED CONSTRUCTOR
+    City(int n){
+        id = n;
+        cout<<"PARAMETERIZED ID is : "<<id<<endl;
+    }
+
+    //COPY CONSTRUCTOR
+    City(City &obj){
+        
+    }
+
+    // //DESTRUCTOR
+    // ~City(){
+    //     cout<<"Object is destroyed...."<<endl;
+    // }
+};
+
+
 int main(){
-    
+    City c1;
+    City c2(3);
+    City c3(c2);
 }
