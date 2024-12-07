84ebc5370d8ab940bb2f75fb1127a92d5307d416
akshar51 <aksharparekh401@gmail.com>
2024-12-07 18:28:59 +0530

    done

---
+++ Cpp/Password 9.2/tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+ // for(i=0;i<length;i++){
+    //    if(pass[i]>='A' || pass[i]<='Z'){
+    //             cp++;
+    //         }
+    //         else if(pass[i]>='a' || pass[i]<='z'){
+    //             sm++;
+    //         }
+    //         else if(pass[i]>=49 || pass[i]<=57){
+    //             num++;
+    //         }
+    //         else if(pass[i]==64){
+    //             spch++;
+    //         }
+    //         else{
+    //             ct=0;
+    //         } 
+    // }
