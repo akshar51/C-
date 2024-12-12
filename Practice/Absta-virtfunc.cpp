a9d2b038f50c239688f5872f9d8dcc6f042d1932
akshar51 <aksharparekh401@gmail.com>
2024-12-12 12:39:45 +0530

    done

--- Practice/Absta-virtfunc.cpp
+++ Practice/Absta-virtfunc.cpp
@@ -3,46 +3,74 @@ using namespace std;
 
 
 // ABSTRACT CLASS AND PURE VIRTUAL FUNCTION
-class A{
+// class A{
+//     public:
+
+//     // PURE VIRTUAL FUNCTON
+//     virtual void movephone()=0; 
+//     virtual void sidephone()=0; 
+
+//     // CONCRETE METHOD
+//     void calling(){
+//         cout<<"Calling"<<endl;
+//     }
+// };
+
+// class B : public A{
+//     public:
+//     void movephone(){
+//         cout<<"Movephone"<<endl;
+//     }
+//     void sidephone(){
+//         cout<<"Sidephone"<<endl;
+//     }
+
+// };
+
+// class C : public B{
+//     public:
+//     void movephone(){
+//         cout<<"phone"<<endl;
+//     }
+// };
+
+class Vehicle{
     public:
-
-    // PURE VIRTUAL FUNCTON
-    virtual void movephone()=0; 
-    virtual void sidephone()=0; 
-
-    // CONCRETE METHOD
-    void calling(){
-        cout<<"Calling"<<endl;
+    virtual void show(){
+        cout<<"Running"<<endl;
     }
 };
 
-class B : public A{
+class Car : public Vehicle{
     public:
-    void movephone(){
-        cout<<"Movephone"<<endl;
-    }
-    void sidephone(){
-        cout<<"Sidephone"<<endl;
-    }
-
-};
-
-class C : public B{
-    public:
-    void movephone(){
-        cout<<"phone"<<endl;
+    void show(){
+        cout<<"Car is red"<<endl;
     }
 };
 
-
+class Bike : public Car{
+    public:
+    void show(){
+        cout<<"Bike is blue"<<endl;
+    }
+};
 
 
 int main(){
 
-    C a;
-    a.calling();
-    a.movephone();
-    a.sidephone();
+    // C a;
+    // a.calling();
+    // a.movephone();
+    // a.sidephone();
+
+
+    Vehicle* a;
+    Car b;
+    Bike c;
+    a=&c;
+    a->show();
+    b.show();
+
 
 
    
