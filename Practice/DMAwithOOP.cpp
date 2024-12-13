c8ad4f5fe522cc4aa29918965c7d944d6d2d7ec2
akshar51 <aksharparekh401@gmail.com>
2024-12-13 17:42:29 +0530

    DONE

--- Practice/DMAwithOOP.cpp
+++ Practice/DMAwithOOP.cpp
@@ -31,12 +31,37 @@ class Myclass{
     }
   }  
 
-  void add
+    //INSERTING ELEMENT ------------CRUD----------- 
+    void addelement(int index,int ele){
+        if(index>=0 && index<this->size){
+            arr[index]=ele;
+            cout<<"Element added successfull"<<endl;
+        }
+        else{
+            cout<<"Error in index position...."<<endl;
+        }
+  }
+
+  // DISPLAY ELEMENT AT INDEX  --------CRUD----------
+
+  void getelement(int index){
+    if(index>=0 && index<this->size){
+        cout<<"Value at index "<<index<<" is "<<arr[index]<<endl;
+    }
+    else{
+        cout<<"Error in index"<<endl;
+    }
+  }
 };
 
 
 
 int main(){
+
+    int arr[]={1,2,3,4};
+   // cout<<arr[0];
     Myclass obj(5);
+    obj.addelement(4,99);
+    obj.getelement(0);
 
 }
