06f20c4305a6982b4105e5fb3e124711cc23a16f
akshar51 <aksharparekh401@gmail.com>
2024-12-20 17:23:01 +0530

    done

--- Cpp/Linked list-Singly/singly.cpp
+++ Cpp/Linked list-Singly/singly.cpp
@@ -19,6 +19,13 @@ int main(){
     a *n2=new a();
     n2->data=50;
     n2->next=NULL;
+    head->next=n2;
 
+
+    cout<<"Data of node 1 is "<<head->data<<endl;
+    cout<<"Address of node 1 is "<<head->next<<endl;
+    cout<<"Data of node 2 is "<<n2->data<<endl;
+    cout<<"Address of node 2 is "<<n2->next<<endl;
+    cout<<n2;
 
 }
