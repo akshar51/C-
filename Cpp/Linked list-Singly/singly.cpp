1f6fb89ceaaa8e0003c862f92e809924a2d613e6
akshar51 <aksharparekh401@gmail.com>
2024-12-20 17:47:15 +0530

    done

--- Cpp/Linked list-Singly/singly.cpp
+++ Cpp/Linked list-Singly/singly.cpp
@@ -13,6 +13,7 @@ int main(){
     
 
     // STATIC WAY TO INITIALIZE LINKED LIST
+    a b;
     a *head=NULL;
     head=new a();
     head->data=25;
@@ -23,11 +24,20 @@ int main(){
     n2->next=NULL;
     head->next=n2;
 
+    a *n3=new a();
+    n3->data=75;
+    n3->next=NULL;
+    n2->next=n3;
+
 
     cout<<"Data of node 1 is "<<head->data<<endl;
     cout<<"Address of node 1 is "<<head->next<<endl;
     cout<<"Data of node 2 is "<<n2->data<<endl;
     cout<<"Address of node 2 is "<<n2->next<<endl;
-    cout<<n2;
+    cout<<"Data of node 3 is "<<n3->data<<endl;
+    cout<<"Address of node 3 is "<<n3->next<<endl;
+    cout<<head<<endl;
+    cout<<n2<<endl;
+    cout<<n3<<endl;
 
 }
