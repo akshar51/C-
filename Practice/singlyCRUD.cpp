6b0f06f895df62cd33c51060860aa0f167d6f033
akshar51 <aksharparekh401@gmail.com>
2024-12-27 13:31:07 +0530

    done

--- Practice/singlyCRUD.cpp
+++ Practice/singlyCRUD.cpp
@@ -23,6 +23,7 @@ class linklist{
         count=0;
     }
 
+    // ADD AN ELEMENT
     void addbegin(int ele){
         node*temp=new node(ele);
         temp->next=head;
@@ -63,6 +64,8 @@ class linklist{
        }
     }
 
+
+    // UPDATE
     void update(int index,int ele){
         node* temp=new node(ele);
         if(index<=0 || index>this->count){
@@ -77,6 +80,60 @@ class linklist{
         }
     }
 
+    // DELETE
+
+    void deletestart(){
+        if(this->head==NULL || this->count==0){
+            cout<<"List is empty...";
+            return;
+        }
+        else{
+            node* temp=this->head;
+            this->head=temp->next;
+            delete temp;
+            temp=NULL;
+        }
+    }
+
+    void deleteend(){
+        if(this->head==NULL || this->count==0){
+            cout<<"List is empty...";
+            return;
+        }
+        else{
+            node* temp=this->head;
+            while(temp->next->next!=NULL){
+                temp=temp->next;
+            }
+            delete temp->next;
+            temp->next=NULL;
+
+        }
+    }
+
+    void deletePos(int index){
+        if(this->head==NULL || this->count==0){
+            cout<<"List is empty...";
+            return;
+        }
+        else{
+            node* prev=this->head;
+            node* temp=this->head;
+            for(int i=1;i<index;i++){
+                temp=temp->next;
+            }
+            for(int i=1;i<index-1;i++){
+                prev=prev->next;
+            }
+            prev->next=temp->next;
+            delete temp;
+            temp=NULL;
+            this->count--;
+        }
+    }
+
+
+    // SHOW
     void show(){
         node* ptr=head;
         while(ptr!=NULL){
@@ -97,7 +154,13 @@ int main(){
         cout<<"Press 2 for add an element at end."<<endl;
         cout<<"Press 3 for add an element at any position."<<endl;
         cout<<"Press 4 for update an element."<<endl;
+        cout<<"Press 5 for delete an element at start."<<endl;
+        cout<<"Press 6 for delete an element at end."<<endl;
+        cout<<"Press 7 for delete an element at any position."<<endl;
         cout<<"Press 8 for display all element."<<endl;
+        cout<<"Press 0 for exit."<<endl;
+
+        
         cout<<"Enter your choice : ";
         cin>>choice;
 
@@ -107,6 +170,7 @@ int main(){
             cin>>ele;
             cout<<endl;
             a.addbegin(ele);
+            cout<<"Element added successfully...."<<endl;
             break;
 
             case 2:
@@ -114,6 +178,7 @@ int main(){
             cin>>ele;
             cout<<endl;
             a.addend(ele);
+            cout<<"Element added successfully...."<<endl;
             break;
 
             case 3:
@@ -123,6 +188,7 @@ int main(){
             cin>>ele;
             cout<<endl;
             a.addtopos(pos,ele);
+            cout<<"Element added successfully...."<<endl;
             break;
 
             case 4:
@@ -134,9 +200,36 @@ int main(){
             a.update(pos,ele);
             break;
 
+            case 5:
+            cout<<endl;
+            a.deletestart();
+             cout<<"Element deleted successfully...."<<endl;
+            break;
+
+            case 6:
+            cout<<endl;
+            a.deleteend();
+            cout<<"Element deleted successfully...."<<endl;
+            break;
+
+            case 7:
+            cout<<"Enter position value : ";
+            cin>>pos;
+            cout<<endl;
+            a.deletePos(pos);
+            cout<<"Element deleted successfully...."<<endl;
+            break;
+
             case 8:
             a.show();
             cout<<endl;
+            break;
+
+            case 0:
+            break;
+
+            default:
+            cout<<"Invalid choice...."<<endl;
             break;
         }
     }while(choice!=0);
