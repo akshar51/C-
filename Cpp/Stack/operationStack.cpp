b39c3c2e9b26515f68f1295c8988dd40097de83c
akshar51 <aksharparekh401@gmail.com>
2024-12-27 17:43:00 +0530

    done

--- Cpp/Stack/operationStack.cpp
+++ Cpp/Stack/operationStack.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 
+
+// OPERATION OF STACK
 class Stack{
     private:
     int* arr;
@@ -9,6 +11,7 @@ class Stack{
     int count;
 
     public:
+    // CONSTRUCTOR
     Stack(int capacity){
         this->capacity=capacity;
         arr=new int[capacity];
@@ -16,40 +19,147 @@ class Stack{
         this->count=0;
     }
 
-    void push(){
+    // PUSHING (ADD AN ELEMENT AT TOP OF STACK)
+    void push(int ele){
+        if(this->top==this->capacity-1){
+            cout<<"Stack overflow.."<<endl;
+        }
+        else{
+            this->top++;
+            arr[top]=ele;
+            this->count++;
+        }
+    }
 
+    // POPPING (REMOVE ELEMENT AT TOP OF STACK)
+    void pop(){
+        if(this->top==-1){
+            cout<<"Stack underflow."<<endl;
+        }
+        else{
+            this->top--;
+            this->count--;
+        }
+    }
+
+    // PEEK (GIVES TOP ELEMENT)
+    void peek(){
+        if(this->top==-1){
+            cout<<"Stack underflow"<<endl;
+        }
+        else{
+            cout<<arr[this->top];
+        }
+    }
+
+    // DISPLAY
+    void display(){
+        if(this->top==-1){
+            cout<<"Stack underflow"<<endl;
+        }
+        else{
+            for(int i=top;i>=0;i--){
+                cout<<arr[i]<<" ";
+            }
+        }
+    }
+
+    // EMPTY (CHECK STACK HAS ANY ELEMENT OR NOT)
+    void empty(){
+        if(this->top==-1){
+            cout<<"Stack is empty."<<endl;
+        }
+        else{
+            cout<<"Stack is NOT empty."<<endl;
+        }
+    }
+
+    // FULL (CHECK STACK IS FULL OR NOT)
+    void full(){
+        if(this->top==this->capacity-1){
+            cout<<"Stack is full."<<endl;
+        }
+        else{
+            cout<<"Stack is not full."<<endl;
+        }
+    }
+
+
+    // SIZE (GIVES SIZE OF STACK)
+    void size(){
+        cout<<"Size of stack is "<<this->count<<endl;
     }
 
     
-    void push(){
-        
-    }
-
-    
-    void push(){
-        
-    }
-
-    
-    void push(){
-        
-    }
-
-    
-    void push(){
-        
+    //DESTRUCTOR
+    ~Stack(){
+        delete[] arr;
+        cout<<"Memory deallocation successfull...."<<endl;
     }
 };
 
 
 int main(){
+
+    Stack a(4);
     int choice,ele;
     do{
         cout<<"Press 1 for push."<<endl;
         cout<<"Press 2 for pop."<<endl;
-        cout<<"Press 3 for push."<<endl;
-        cout<<"Press 1 for push."<<endl;
-        cout<<"Press 1 for push."<<endl;
-        cout<<"Press 1 for push."<<endl;
+        cout<<"Press 3 for peek."<<endl;
+        cout<<"Press 4 for empty."<<endl;
+        cout<<"Press 5 for full."<<endl;
+        cout<<"Press 6 for size."<<endl;
+        cout<<"Press 7 for display."<<endl;
+        cout<<"Press 0 for exit."<<endl<<endl;
+
+        cout<<"Enter your choice : ";
+        cin>>choice;
+
+        switch(choice){
+            case 1:
+            cout<<"Enter element : ";
+            cin>>ele;
+            a.push(ele);
+            cout<<endl;
+            break;
+
+            case 2:
+            a.pop();
+            cout<<endl;
+            break;
+
+            case 3:
+            a.peek();
+            cout<<endl;
+            break;
+
+            case 4:
+            a.empty();
+            cout<<endl;
+            break;
+
+            case 5:
+            a.full();
+            cout<<endl;
+            break;
+
+            case 6:
+            a.size();
+            cout<<endl;
+            break;
+
+            case 7:
+            a.display();
+            cout<<endl;
+            break;
+
+            case 0:
+            break;
+
+            default:
+            cout<<"Invalid choice...."<<endl;
+            break;
+        }
     }while(choice!=0);
 }
