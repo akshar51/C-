d2e17acffa0e891e73c910666323e342914fc00a
akshar51 <aksharparekh401@gmail.com>
2024-12-27 12:23:13 +0530

    done

--- Practice/singlyCRUD.cpp
+++ Practice/singlyCRUD.cpp
@@ -27,7 +27,7 @@ class linklist{
         node*temp=new node(ele);
         temp->next=head;
         head=temp;
-        count++;
+        this->count++;
     }
 
     void addend(int ele){
@@ -43,6 +43,38 @@ class linklist{
             ptr->next=temp;
             this->count++;
             }
+    }
+
+    void addtopos(int index,int ele){
+       node* temp=new node(ele);
+       if(index==0){
+        temp->next=NULL;
+        this->head=temp;
+       }
+       else{
+        node* ptr=this->head;
+        node* curr=temp;
+        for(int i=1;i<index-1;i++){
+            ptr=ptr->next;
+        }
+        curr->next=ptr->next;
+        ptr->next=curr;
+        this->count++;
+       }
+    }
+
+    void update(int index,int ele){
+        node* temp=new node(ele);
+        if(index<=0 || index>this->count){
+            cout<<"Invalid index value...";
+        }
+        else{
+            node* ptr=head;
+            for(int i=1;i<index;i++){
+                ptr=ptr->next;
+            }
+            ptr->data=ele;
+        }
     }
 
     void show(){
@@ -63,6 +95,8 @@ int main(){
     do{
         cout<<"Press 1 for add an element at begin."<<endl;
         cout<<"Press 2 for add an element at end."<<endl;
+        cout<<"Press 3 for add an element at any position."<<endl;
+        cout<<"Press 4 for update an element."<<endl;
         cout<<"Press 8 for display all element."<<endl;
         cout<<"Enter your choice : ";
         cin>>choice;
@@ -80,6 +114,24 @@ int main(){
             cin>>ele;
             cout<<endl;
             a.addend(ele);
+            break;
+
+            case 3:
+            cout<<"Enter position value : ";
+            cin>>pos;
+            cout<<"Enter your element : ";
+            cin>>ele;
+            cout<<endl;
+            a.addtopos(pos,ele);
+            break;
+
+            case 4:
+            cout<<"Enter position value : ";
+            cin>>pos;
+            cout<<"Enter your element : ";
+            cin>>ele;
+            cout<<endl;
+            a.update(pos,ele);
             break;
 
             case 8:
