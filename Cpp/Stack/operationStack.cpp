8c2d9910d3a2fb995b720d86f688b0b919fa50c5
akshar51 <aksharparekh401@gmail.com>
2024-12-27 17:09:14 +0530

    done

---
+++ Cpp/Stack/operationStack.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+class Stack{
+    private:
+    int* arr;
+    int capacity;
+    int top;
+    int count;
+
+    public:
+    Stack(int capacity){
+        this->capacity=capacity;
+        arr=new int[capacity];
+        this->top=-1;
+        this->count=0;
+    }
+
+    void push(){
+
+    }
+
+    
+    void push(){
+        
+    }
+
+    
+    void push(){
+        
+    }
+
+    
+    void push(){
+        
+    }
+
+    
+    void push(){
+        
+    }
+};
+
+
+int main(){
+    int choice,ele;
+    do{
+        cout<<"Press 1 for push."<<endl;
+        cout<<"Press 2 for pop."<<endl;
+        cout<<"Press 3 for push."<<endl;
+        cout<<"Press 1 for push."<<endl;
+        cout<<"Press 1 for push."<<endl;
+        cout<<"Press 1 for push."<<endl;
+    }while(choice!=0);
+}
