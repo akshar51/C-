d4f9f7880c936ac234ffe2a26794761abe4fe6fe
akshar51 <aksharparekh401@gmail.com>
2025-01-07 16:50:20 +0530

    done

---
+++ Cpp/BubbleSort/ascendingSort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void bubble(int a[],int n){
+    int pass=1;
+
+    while(pass<=n-1){
+
+    }
+}
+
+void print(int a[],int n){
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter array size : ";
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cout<<"a["<<i<<"] = ";
+        cin>>a[i];
+    }
+
+
+}
