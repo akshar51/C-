4d1b2f9712246562d913e463f1aea1fc4c521ce2
akshar51 <aksharparekh401@gmail.com>
2025-01-08 16:54:51 +0530

    done

---
+++ Cpp/InsertionSort/ascending.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void print(int a[],int n){
+    cout<<"Array is : ";
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+
+}
