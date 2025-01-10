acde01a2c7e6e8a52c0ac17392d9560a6b9a0984
akshar51 <aksharparekh401@gmail.com>
2025-01-10 17:28:41 +0530

    done

--- Cpp/MergeSort/ascending.cpp
+++ Cpp/MergeSort/ascending.cpp
@@ -2,13 +2,41 @@
 #include<vector>
 using namespace std;
 
-void print(vector<int>&a){
-    for(int i=0;i<a.size();i++){
 
+
+// MERGE 2 HALVES OF ARRAY
+void merge(vector<int>&a,int low,int mid,int high){
+
+    vector<int>temp;
+    int left=low;
+    int right=mid+1;
+
+    while(left<=mid && right<=high){
+        if(a[left]<=a[right]){
+            temp.push_back(a[left]);
+            left++;
+        }
+        else{
+            temp.push_back(a[right]);
+            right++;
+        }
+    }
+    while(left<=mid){
+        temp.push_back(a[left]);
+        left++;
+    }
+    while(right<=high){
+        temp.push_back(a[right]);
+        right++;
+    }
+
+    for(int i=low;i<=high;i++){
+        a[i]=temp[i-low];
     }
 }
 
 
+// DIVIDE ARRAY INTO 2 PART
 void mergesort(vector<int>&a,int low,int high){
     if(low>=high){
         return;
@@ -25,23 +53,13 @@ void mergesort(vector<int>&a,int low,int high){
 
 }
 
-void merge(vector<int>&a,int low,int mid,int high){
-    vector<int>temp;
-    int left=low;
-    int right=mid+1;
-
-    while(left<=mid && right<=high){
-        if(a[left]<=a[right]){
-            temp.push_back(a[left]);
-            left++;
-        }
-        else{
-            temp.push_back(a[right]);
-            right++;
-        }
+void print(vector<int>&a,int n){
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
     }
-    while(left<=mid)
+    cout<<endl;
 }
+
 
 
 
@@ -56,8 +74,9 @@ int main(){
         cin>>element;
         a.push_back(element);
     }
-
+    cout<<"Array is : ";
     print(a,n);
     mergesort(a,0,n-1);
+    cout<<"Sorted array is : ";
     print(a,n);
 }
