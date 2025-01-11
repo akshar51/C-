a5ee848fc63c256a4f9f64a1002b7b1fbad693c2
akshar51 <aksharparekh401@gmail.com>
2025-01-11 16:35:39 +0530

    done

--- Practice/week3.cpp
+++ Practice/week3.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 using namespace std;
 
-void bubbleSort(){
+void bubbleSort(int ar[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(ar[j]>ar[j+1]){
+                swap(ar[j],ar[j+1]);
+            }
+        }
+    }
 
 }
 void print(int ar[],int n){
