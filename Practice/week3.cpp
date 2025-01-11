af0f6d70bcfadf6a1e71e1f0e43d9b2465285be3
akshar51 <aksharparekh401@gmail.com>
2025-01-11 16:50:28 +0530

    done

--- Practice/week3.cpp
+++ Practice/week3.cpp
@@ -1,16 +1,31 @@
 #include<iostream>
 using namespace std;
+// BUBBLE SORT
+// void bubbleSort(int ar[],int n){
+//     for(int i=0;i<n-1;i++){
+//         for(int j=0;j<n-i-1;j++){
+//             if(ar[j]>ar[j+1]){
+//                 swap(ar[j],ar[j+1]);
+//             }
+//         }
+//     }
 
-void bubbleSort(int ar[],int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(ar[j]>ar[j+1]){
-                swap(ar[j],ar[j+1]);
-            }
+// }
+
+// INSERTION SORT
+void insertionSort(int ar[],int n){
+
+    for(int i=1;i<n-1;i++){
+        int key=ar[i];//10
+        int j=i-1;//20
+        while(j>=0 && key<ar[j]){
+            ar[j]=ar[j+1];
+            j--;
         }
+        ar[j+1]=key;
     }
-
 }
+
 void print(int ar[],int n){
     for(int i=0;i<n;i++){
         cout<<ar[i]<<" ";
@@ -30,7 +45,8 @@ int main(){
     cout<<endl;
     cout<<"Array is : ";
     print(ar,n);
-    bubbleSort(ar,n);
+    insertionSort(ar,n);
+    //bubbleSort(ar,n);
     cout<<"Sorted array is : ";
     print(ar,n);
     return 0;
