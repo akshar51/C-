0b8d688632690fa75851277f09e51fe8506382a8
akshar51 <aksharparekh401@gmail.com>
2025-01-13 14:21:20 +0530

    done

--- Practice/week3.cpp
+++ Practice/week3.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 // BUBBLE SORT
@@ -53,6 +54,52 @@ using namespace std;
 //     return 0;
 // }
 
+
+void merge(int ar[],int start,int mid,int end){
+    vector<int> temp;
+    int left=start;
+    int right=mid+1;
+    int index=0;
+    while(left<=mid && right<=end){
+        if(ar[left]<=ar[right]){
+            temp[index]=ar[left];
+            left++;
+            index++;
+        }
+        else{
+            temp[index]=ar[right];
+            right++;
+            index++;
+        }
+    }
+    while(left<=mid){
+        temp[index=ar[left];
+    }
+    while(right<=end){
+        temp[index]=ar[right];
+    }
+}
+
 void mergesort(int ar[],int start,int end){
-    
+    if(start==end){
+        return;
+    }
+    int mid=start+end/2;
+    mergesort(ar,start,mid);
+    mergesort(ar,mid+1,end);
+    merge(ar,start,mid,end);
+}
+
+void print(int ar[],int size){
+    for(int i=0;i<size;i++){
+        cout<<ar[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int ar[]={34,15,17,5,1,3};
+    mergesort(ar,0,ar.size()-1);
+    return 0;
+
 }
