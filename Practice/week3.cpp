bb5b9c73714d2cf0b5e83683b6e409909f590e2a
akshar51 <aksharparekh401@gmail.com>
2025-01-13 14:09:34 +0530

    done

--- Practice/week3.cpp
+++ Practice/week3.cpp
@@ -14,41 +14,45 @@ using namespace std;
 // }
 
 // INSERTION SORT
-void insertionSort(int ar[],int n){
+// void insertionSort(int ar[],int n){
 
-    for(int i=1;i<n;i++){
-        int key=ar[i];
-        int j=i-1;
-        while(j>=0 && key<ar[j]){
-            ar[j+1]=ar[j];
-            j--;
-        }
-        ar[j+1]=key;
-    }
-}
+//     for(int i=1;i<n;i++){
+//         int key=ar[i];
+//         int j=i-1;
+//         while(j>=0 && key<ar[j]){
+//             ar[j+1]=ar[j];
+//             j--;
+//         }
+//         ar[j+1]=key;
+//     }
+// }
 
-void print(int ar[],int n){
-    for(int i=0;i<n;i++){
-        cout<<ar[i]<<" ";
-    }
-    cout<<endl;
-}
+// void print(int ar[],int n){
+//     for(int i=0;i<n;i++){
+//         cout<<ar[i]<<" ";
+//     }
+//     cout<<endl;
+// }
 
-int main(){
-    int n;
-    cout<<"Enter array size :  ";
-    cin>>n;
-    int ar[n];
-    for(int i=0;i<n;i++){
-        cout<<"ar["<<i<<"] = ";
-        cin>>ar[i];
-    }
-    cout<<endl;
-    cout<<"Array is : ";
-    print(ar,n);
-    insertionSort(ar,n);
-    //bubbleSort(ar,n);
-    cout<<"Sorted array is : ";
-    print(ar,n);
-    return 0;
+// int main(){
+//     int n;
+//     cout<<"Enter array size :  ";
+//     cin>>n;
+//     int ar[n];
+//     for(int i=0;i<n;i++){
+//         cout<<"ar["<<i<<"] = ";
+//         cin>>ar[i];
+//     }
+//     cout<<endl;
+//     cout<<"Array is : ";
+//     print(ar,n);
+//     insertionSort(ar,n);
+//     //bubbleSort(ar,n);
+//     cout<<"Sorted array is : ";
+//     print(ar,n);
+//     return 0;
+// }
+
+void mergesort(int ar[],int start,int end){
+    
 }
