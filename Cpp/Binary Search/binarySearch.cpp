0a8d9f62e07949ddab879dbb989e58505b398a15
akshar51 <aksharparekh401@gmail.com>
2025-01-17 16:59:33 +0530

    done

--- Cpp/Binary Search/binarySearch.cpp
+++ Cpp/Binary Search/binarySearch.cpp
@@ -23,7 +23,19 @@ int main(){
         cin>>a[i];
     }
     cout<<endl;
+    cout<<"Array element is : ";
     print(a,n);
     cout<<"Enter your target : ";
-    
+    cin>>target;
+    int founded=binary_search(a,n,0,n-1,target);
+
+    if(founded==-1){
+        cout<<"Element not founded...."<<endl;
+    }
+    else{
+        cout<<"Element founded...."<<endl;
+    }
+
+
+    return 0;
 }
